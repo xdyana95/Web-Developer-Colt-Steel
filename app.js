87e86e728edb5a8083120e8d6beeb55b4d21ccc2
xdyana95 <42338238+xdyana95@users.noreply.github.com>
2019-02-03 21:58:13 -0700

    Add files via upload
    movie search app learning about APIs

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var express = require("express");
-var app = express();
+var app = express ();
+var request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.send("Hi there!");
-});
-app.get("/bye", function(req, res) {
-    res.send("Goodbye!");
-});
-app.get("/dog", function(req, res) {
-    console.log("Someone made a request to dog");
-    res.send("meow!");
+    res.render("search");
 });
 
-app.get("*", function(req, res) {
-    res.send("You are a star!");
+app.get("/results", function(req, res){
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render("results", {data: data});
+        }
+    });
 });
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("Server has started!!");
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Movie App has started!!!");
 });
